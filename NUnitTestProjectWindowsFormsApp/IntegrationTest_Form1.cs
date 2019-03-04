36b928cfbd1f5e91a3fbf02ad39124a3f4b92c44
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2019-03-04 19:38:34 +0300

    Добавил проект NUnitTest для игтеграционного тестирования.

---
+++ NUnitTestProjectWindowsFormsApp/IntegrationTest_Form1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using WindowsFormsApp;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
