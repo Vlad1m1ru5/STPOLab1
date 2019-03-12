2c811b7b7b13e6ad4b268ffae6b9bb0eea5566c4
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2019-03-12 22:55:03 +0300

    Создан coded user interface test.

---
+++ CodedeUserInterfaceTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp;
+using AutoItX3Lib;
+
+namespace CodedeUserInterfaceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_WindowsFormsApp()
+        {
+            AutoItX3 au3 = new AutoItX3();
+        }
+    }
+}
